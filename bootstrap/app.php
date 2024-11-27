e2005010aec331192b0af15be088680762186879
O.Wael <omarwael19994@yahoo.com>
2024-11-27 11:23:49 +0200

    some edits

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->global(function (Request $request, Closure $next) {
+            if (!$request->isSecure() && app()->environment('production')) {
+                return redirect()->secure($request->getRequestUri());
+            }
+
+            return $next($request);
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
